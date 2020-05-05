c776874091a69682f44a2a80814e56c95e966384
afu <ylp1127@126.com>
2020-05-05 15:56:39 +0800

    change doc

--- js.js
+++ js.js
@@ -3,12 +3,13 @@
 var nav = document.getElementById('rightnav');
 var width = nav.clientWidth;
 var height = nav.clientHeight;
+var winWidth = window.innerWidth;
 var winHeight = window.innerHeight;
 
 nav.style.position = 'fixed';
 nav.style.width = width + 'px';
 
-if(height + 70 > winHeight) {
+if(winWidth > 1200 && height + 70 > winHeight) {
     window.addEventListener('scroll', () => {
         var top = document.documentElement.scrollTop || document.body.scrollTop;
         if(top > 100) {
